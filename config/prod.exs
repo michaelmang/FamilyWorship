2d99f57ab55a5541a8ee9252125dd879f8694c83
Mike Mangialardi <mmangialardi@vigilantsys.com>
2017-08-12 00:52:31 -0400

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :family_worship, FamilyWorshipWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "sheltered-mesa-51446.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
