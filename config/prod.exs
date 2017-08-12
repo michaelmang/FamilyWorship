c812f821eaae0233e2ef72d517713dc14962c27e
Mike Mangialardi <mmangialardi@vigilantsys.com>
2017-08-12 01:27:52 -0400

    Updated endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :family_worship, FamilyWorshipWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "sheltered-mesa-51446.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
