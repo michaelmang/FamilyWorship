460768cb9f8ab91356c461ad980f7e9fd4750a6a
Mike Mangialardi <mmangialardi@vigilantsys.com>
2017-08-12 01:38:09 -0400

    Updated buildpack

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule FamilyWorship.Mixfile do
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
       deps: deps()
